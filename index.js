c809b765488b042a366a537c754c845fe8b1026d
tatiana-gary <tatiana.garay218871@alumnos.info.unlp.edu.ar>
2023-12-12 19:34:13 -0300

    cambios

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const menu = document.querySelector('.li-container');
 const navmenu = document.querySelector('.burger-btn');
 
 navmenu.onclick= function(){
-    menu.classList.toggle('active')
     navmenu.classList.toggle('active')
+    menu.classList.toggle('active')
+    
 }
